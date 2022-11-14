2631d23995522bdebefce151111dc1c319b54bdc
Marlowe Recht <recht.marlowe@gmail.com>
2022-11-14 23:29:53 +0100

    commit tree 1e366602aee6ed24eafd0b2d9888c282557f9425

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat() {
+    const byeCat = cats.slice(0, 2);
+    return byeCat;
+}
+
+function removeFirstCat() {
+    const byeAgain = cats.slice(1);
+    return byeAgain;
+}
